7c7b40a8b13c87986c3bbd2425075a8c08e52e63
ecyamak <ensarcanyamak@gmail.com>
2018-07-08 21:37:20 +0300

    Initialize posting and fetching from database completed

--- app/src/main/java/com/example/user/calendar/NewAppointmentActivity.java
+++ app/src/main/java/com/example/user/calendar/NewAppointmentActivity.java
@@ -2,12 +2,70 @@ package com.example.user.calendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class NewAppointmentActivity extends AppCompatActivity {
+
+    Button add;
+    TextView title, description, date, time;
+    String dbURL;
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_appointment);
+
+        add = findViewById(R.id.button4);
+        title = findViewById(R.id.editText9);
+        description = findViewById(R.id.editText10);
+        date = findViewById(R.id.editText11);
+        time = findViewById(R.id.editText12);
+        dbURL = "https://honest-eh-63187.herokuapp.com/appointments";
+
+        queue = Volley.newRequestQueue(this);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StringRequest request = new StringRequest(Request.Method.POST, dbURL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }){
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> parameters = new HashMap<String, String>();
+                        parameters.put("title", title.getText().toString());
+                        parameters.put("description", description.getText().toString());
+                        parameters.put("event_date", date.getText().toString());
+
+                        return parameters;
+                    }
+                };
+                queue.add(request);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/user/calendar/UpcomingEventsActivity.java
+++ app/src/main/java/com/example/user/calendar/UpcomingEventsActivity.java
@@ -1,14 +1,76 @@
 package com.example.user.calendar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class UpcomingEventsActivity extends AppCompatActivity {
+
+    Button buttonRefresh;
+    TextView mTextViewResult;
+    RequestQueue queue;
+    String dbURL, result, title, description, event_date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upcoming_events);
 
+        Intent intent = getIntent();
+        mTextViewResult = findViewById(R.id.textView);
+        buttonRefresh = findViewById(R.id.button5);
+        String keyword = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        mTextViewResult.setText(keyword);
+
+        queue = Volley.newRequestQueue(this);
+        buttonRefresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jsonParse();
+            }
+        });
+    }
+
+    private void jsonParse(){
+        dbURL = "https://honest-eh-63187.herokuapp.com/appointments";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, dbURL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                result = "";
+                try {
+                    JSONArray appointments = new JSONArray(response);
+                    for(int i = 0; i < appointments.length(); i++){
+                        JSONObject appointment = (JSONObject) appointments.get(i);
+                        title = appointment.getString("title");
+                        description = appointment.getString("description");
+                        event_date = appointment.getString("event_date");
+                        result += title + " " + description + " " + event_date + "\n";
+                    }
+                } catch (JSONException e) {
+                    mTextViewResult.setText("404 NOT FOUND!");
+                }
+                mTextViewResult.setText(result);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextViewResult.setText("404 NOT FOUND!");
+            }
+        });
+        queue.add(stringRequest);
     }
 }
