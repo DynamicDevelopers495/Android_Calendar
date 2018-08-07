704d648c621af137c1946e1d92cd769faaf6c7c9
ecyamak <ensarcanyamak@gmail.com>
2018-08-07 18:55:00 +0300

    Veritabanına veri yükleme

--- app/src/main/java/com/example/user/calendar/NewAppointmentActivity.java
+++ app/src/main/java/com/example/user/calendar/NewAppointmentActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.calendar;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,8 +15,17 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,35 +46,43 @@ public class NewAppointmentActivity extends AppCompatActivity {
         description = findViewById(R.id.editText10);
         date = findViewById(R.id.editText11);
         time = findViewById(R.id.editText12);
-        dbURL = "https://honest-eh-63187.herokuapp.com/appointments";
+        dbURL = "http://arcane-hollows-92806.herokuapp.com/appointments";
 
         queue = Volley.newRequestQueue(this);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                StringRequest request = new StringRequest(Request.Method.POST, dbURL, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
+                try{
+                    URL url = new URL(dbURL);
+                    HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+                    httpCon.setDoOutput(true);
+                    httpCon.setRequestProperty("Content-Type", "application/x-www-form-urlencoded" );
+                    httpCon.setRequestMethod("POST");
+                    httpCon.setRequestProperty("Content-Type","application/json");
+                    JSONObject eventObject = new JSONObject();
+                    eventObject.put("title",title.getText().toString());
+                    eventObject.put("description", description.getText().toString());
+                    eventObject.put("event_date", date.getText().toString()+time.getText().toString());
+                    String json = eventObject.toString();
 
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
+                    byte[] outputInBytes = json.getBytes("UTF-8");
+                    OutputStream os = httpCon.getOutputStream();
+                    os.write( outputInBytes );
+                    os.close();
 
-                    }
-                }){
-                    @Override
-                    protected Map<String, String> getParams() throws AuthFailureError {
-                        Map<String, String> parameters = new HashMap<String, String>();
-                        parameters.put("title", title.getText().toString());
-                        parameters.put("description", description.getText().toString());
-                        parameters.put("event_date", date.getText().toString());
-
-                        return parameters;
-                    }
-                };
-                queue.add(request);
+                    int responseCode = httpCon.getResponseCode();
+                    System.out.println("response code: " + responseCode);
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
